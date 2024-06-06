a14a0510fd1e4520b7771fb35af1d5a1644a5521
S-Mukherjee98 <shubhramukherjee4@gmail.com>
2024-06-07 00:51:10 +0530

    Efficient solution

--- DSA_WITH_JAVA/Most_Common_Problems/Problem8.java
+++ DSA_WITH_JAVA/Most_Common_Problems/Problem8.java
@@ -35,6 +35,9 @@ public class Problem8 {
     public int  removeDuplicates2(int arr[],int n){
         Set<Integer> unique_elements = new HashSet<>();
         int i ;
+
+
+        if (n == 0 || n == 1)return n;
         for (i = 0; i < n; i++) {
             unique_elements.add(arr[i]);
         }
@@ -45,12 +48,27 @@ public class Problem8 {
         return unique_elements.size();
     }
 
+    public int  removeDuplicates3(int arr[],int n){
+
+        int index = 0;
+
+        if (n == 0 || n == 1)return n;
+
+        for (int i = 0; i < n-1; i++) {
+            if (arr[i]!=arr[i+1]) {
+                arr[index++]=arr[i];
+            }
+        }
+        arr[index++]=arr[n-1];
+        return index;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 1, 2, 2, 3, 4, 4, 4, 5, 5 };
         int n = arr.length;
         Problem8 obj=new Problem8();
         // obj.removeDuplicates(arr, n);
-        int res=obj.removeDuplicates2(arr, n);
+        int res=obj.removeDuplicates3(arr, n);
         for (int i = 0; i < res; i++) {
             System.out.print(" "+arr[i]);
         }
