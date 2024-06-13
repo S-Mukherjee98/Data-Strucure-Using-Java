f701db93461d35db6b30ee0a26cfdcfd9f509a18
S-Mukherjee98 <shubhramukherjee4@gmail.com>
2024-06-13 23:08:46 +0530

    Intersection of 2 linkedlist

--- DSA_WITH_JAVA/Most_Common_Problems/Linked_List_Basic_Problems/LLProblem2.java
+++ DSA_WITH_JAVA/Most_Common_Problems/Linked_List_Basic_Problems/LLProblem2.java
@@ -2,6 +2,12 @@ package DSA_WITH_JAVA.Most_Common_Problems.Linked_List_Basic_Problems;
 
 //Find the intersection point of two linked lists
 
+import java.util.HashSet;
+
+
+
+
+
 public class LLProblem2 {
     static class Node{
         int data;
@@ -12,6 +18,10 @@ public class LLProblem2 {
         }
     }
 
+    /*
+ * In this apprach the time complexity of the algorithm is O(m*n)
+ * where m and n are the lengths of the two linked lists.
+ */
     public Node getIntersectionNode(Node headA, Node headB) {
         // Write your code here.
         Node curr1=headA;
@@ -26,7 +36,35 @@ public class LLProblem2 {
         }
         return null;
     }
+
+
+    //Find intersection of two linked lists using hash set
+    //Time complexity is O(n) where n is the length of longer linked list.
+    public Node getIntersectionNode2(Node headA, Node headB ){
+        HashSet<Node> hs=new HashSet<Node>();
+        while (headA!=null) {
+            hs.add(headA);
+            headA=headA.next;
+        }
+        while(headB!=null){
+            if(hs.contains(headB)){
+                return headB;
+            }
+            headB=headB.next;
+        }
+        return null;
+    }
    
+    public int getLength(Node head){
+        int len=0;
+        while (head!=null) {
+            len++;
+            head=head.next;
+        }
+        return len;
+    }
+
+    
 
   
     public static void main(String[] args) {
@@ -40,15 +78,16 @@ public class LLProblem2 {
         head1=new Node(10);
         head1.next=new Node(15);
         head1.next.next=new Node(20);
-        head1.next.next.next=new Node(25);
-        head1.next.next.next.next=null;
+        
+       
 
         head2=new Node(5);
         head2.next=new Node(12);
-        head2.next.next=head1.next.next;
-        head2.next.next.next=null;
+        head2.next.next=new Node(30);
+        head2.next.next.next=head1.next; 
+       
 
-        Node intersection_node =llist.getIntersectionNode(head1,head2);
+        Node intersection_node =llist.getIntersectionNode2(head1,head2);
         if (intersection_node!=null) {
             System.out.println("Intersection Node is "+intersection_node.data);
         }
